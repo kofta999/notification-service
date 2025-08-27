2058da8234f3e1f1f4812055936fbd026fc5c197
kofta999 <mostafaxxx555@gmail.com>
2025-08-27 14:30:09 +0300

    feat: add mock email send operation

--- src/worker.ts
+++ src/worker.ts
@@ -1,14 +1,35 @@
+import { setTimeout as sleep } from "node:timers/promises";
 import { db } from ".";
-import { Notification } from "./generated/prisma";
+import type { Notification } from "./generated/prisma";
 
 export async function processNotification(notification: Notification) {
-  // Sends notification
-  console.log("SENDING NOTIFICATION", notification);
+  // Send notification
+  console.log(`Sending notification with ID: ${notification.id}`);
+  try {
+    if (notification.channel === "email") {
+      await sendEmail(notification);
+    }
 
-  // Assume sent successfully
-  // Updates DB with new status
-  await db.notification.update({
-    where: { id: notification.id },
-    data: { status: "SENT" },
-  });
+    await db.notification.update({
+      where: { id: notification.id },
+      data: { status: "SENT" },
+    });
+
+    console.log(`Notification with ID: ${notification.id} is sent`);
+  } catch (error) {
+    console.log(`Failed to send notification with ID: ${notification.id}`);
+    await db.notification.update({
+      where: { id: notification.id },
+      data: { status: "FAILED" },
+    });
+  }
+}
+
+async function sendEmail(notification: Notification) {
+  const sleepDuration = Math.random() * 10000;
+  await sleep(sleepDuration);
+
+  if (sleepDuration >= 5000) {
+    throw new Error("Failed to send email");
+  }
 }
